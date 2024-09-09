c19d7f5791080188e6d9d1d855553921627f019f
Duncan Asplundh <drasplundh@gmail.com>
2024-09-09 15:37:35 -0400

    changed feature 3

--- src/main/java/dev/duncan/test/Controller.java
+++ src/main/java/dev/duncan/test/Controller.java
@@ -15,7 +15,7 @@ public class Controller {
     public String report2() { return "I'm a successful feature";}
 
     @RequestMapping(value = "test3")
-    public String report3() { return "I'm a 3rd feature";}
+    public String report3() { return "NEW FEATURE ADDED!";}
 
 
 }
